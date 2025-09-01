c54341bfc6111f42ab2da7f13be18e289b3b15e5
ProffX <36168576+chainchopper@users.noreply.github.com>
2025-08-31 23:18:27 -0400

    [dyad] Fixed 'process is not defined' error by configuring Webpack - wrote 1 file(s)

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,24 @@
  */
 
 const AngularComponentTagger = require('./angular-webpack-component-tagger');
+const webpack = require('webpack');
+require('dotenv').config();
 
 module.exports = {
   plugins: [
-    new AngularComponentTagger()
+    new AngularComponentTagger(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        TTS_API_ENDPOINT: JSON.stringify(process.env.TTS_API_ENDPOINT),
+        TTS_API_KEY: JSON.stringify(process.env.TTS_API_KEY),
+        SUGGESTIONS_API_ENDPOINT: JSON.stringify(process.env.SUGGESTIONS_API_ENDPOINT),
+        SUGGESTIONS_API_KEY: JSON.stringify(process.env.SUGGESTIONS_API_KEY),
+        METADATA_API_ENDPOINT: JSON.stringify(process.env.METADATA_API_ENDPOINT),
+        METADATA_API_KEY: JSON.stringify(process.env.METADATA_API_KEY),
+        OPENAI_API_BASE_URL: JSON.stringify(process.env.OPENAI_API_BASE_URL),
+        OPENAI_API_KEY: JSON.stringify(process.env.OPENAI_API_KEY),
+        VISION_MODEL_NAME: JSON.stringify(process.env.VISION_MODEL_NAME),
+      }
+    })
   ]
 };
