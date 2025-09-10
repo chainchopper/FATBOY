6e45d3cba6bc50a64ca148df44db44973f709393
ProffX <36168576+chainchopper@users.noreply.github.com>
2025-09-10 12:10:14 -0400

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

---
+++ src/polyfills.ts
@@ -0,0 +1,17 @@
+import 'zone.js'; // Included with Angular CLI.
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/**
+ * If you need to add more polyfills, add them here.
+ * For example, for IE 11 support, you might need:
+ * import 'core-js/es/array';
+ * import 'core-js/es/map';
+ * import 'core-js/es/set';
+ * import 'core-js/es/string';
+ * import 'core-js/es/object';
+ * import 'core-js/es/promise';
+ * import 'core-js/es/symbol';
+ */
