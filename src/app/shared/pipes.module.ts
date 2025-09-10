19f4a6b414b460f98aa4afc6e0797646fb67defd
ProffX <36168576+chainchopper@users.noreply.github.com>
2025-09-10 12:49:50 -0400

    [dyad] wrote 1 file(s), deleted 1 file(s) + extra files edited outside of Dyad

--- src/app/shared/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, TitleCasePipe } from '@angular/common';
-
-@NgModule({
-  imports: [CommonModule],
-  exports: [CommonModule, TitleCasePipe]
-})
-export class PipesModule { }
